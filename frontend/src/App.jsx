7a8a413f27cbdf8347817881ec1c3f292d9b848e
Ranjith H K <git@github.com:ranjith0903/mern.git>
2025-01-04 22:02:57 +0530

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,8 +12,8 @@ import FindCampaign from "./pages/FindCampaign";
 import { useUserStore } from "./store/useUserStore";
 import ProfilePage from "./pages/ProfilePage";
 import RegisterCampaign from "./pages/RegisterCampaign";
+import AdminPage from "./pages/AdminPage"
 
-import AdminPage from "./pages/adminPage";
 import { DeleteUser } from "./pages/DeleteUser";
 
 import DeleteCampaign from "./pages/DeleteCampaign";
@@ -22,20 +22,21 @@ import { BloodStatsPage } from "./pages/BloodStatsPage";
 import About from "./pages/About";
 import { Toaster } from "react-hot-toast";
 
-
 function App() {
   const { user, checkAuth, checkingAuth } = useUserStore();
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
-  
 
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
-        <Route path="/profile" element={!user?<LoginPage/>:<ProfilePage />}></Route>
+        <Route
+          path="/profile"
+          element={!user ? <LoginPage /> : <ProfilePage />}
+        ></Route>
         <Route
           path="/login"
           element={!user ? <LoginPage /> : <Navigate to="/" />}
@@ -48,19 +49,39 @@ function App() {
           path="/signup"
           element={!user ? <SignUpPage /> : <Navigate to="/" />}
         ></Route>
-      
+
         <Route path="/findDonors" element={<FindDonors />}></Route>
         <Route path="/findCampaign" element={<FindCampaign />}></Route>
-        <Route path="/findStats" element={user?.role==="admin"?<FindStats />:<Navigate to="/" />}></Route>
-       
+        <Route
+          path="/findStats"
+          element={user?.role === "admin" ? <FindStats /> : <Navigate to="/" />}
+        ></Route>
+
         <Route path="/registerCampaign" element={<RegisterCampaign />} />
 
-        <Route path="/admin" element={user?.role==="admin"?<AdminPage />:<Navigate to="/" />} />
-        <Route path="/deleteuser" element={user?.role==="admin"?<DeleteUser />:<Navigate to="/" />} />
-        <Route path="/deletecampaign" element={user?.role==="admin"?<DeleteCampaign />:<Navigate to="/" />} />
-        <Route path="/bloodstats" element={user?.role==="admin"?<BloodStatsPage/>:<Navigate to="/" />} />
+        <Route
+          path="/admin"
+          element={user?.role === "admin" ? <AdminPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/deleteuser"
+          element={
+            user?.role === "admin" ? <DeleteUser /> : <Navigate to="/" />
+          }
+        />
+        <Route
+          path="/deletecampaign"
+          element={
+            user?.role === "admin" ? <DeleteCampaign /> : <Navigate to="/" />
+          }
+        />
+        <Route
+          path="/bloodstats"
+          element={
+            user?.role === "admin" ? <BloodStatsPage /> : <Navigate to="/" />
+          }
+        />
         <Route path="/about" element={<About />} />
-        
       </Routes>
       <Toaster />
     </>
